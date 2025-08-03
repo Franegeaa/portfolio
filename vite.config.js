51eb3fc751e19cd06797faa2ba6c81017ead0a2e
Franegeaa <egeafran02@gmail.com>
2025-08-03 16:23:32 -0300

    Fix Vercel deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Cambia 'portfolio' por el nombre exacto de tu repositorio
 })
