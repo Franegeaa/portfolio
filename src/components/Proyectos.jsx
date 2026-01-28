cea4edd9b9914be4439d145b75150b69394a8faf
Francisco Egea <egeafran02@gmail.com>
2026-01-28 12:59:05 -0300

    Agrega proyecto abm facu

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -47,6 +47,13 @@ const Proyectos = () => {
         }
       ]
     },
+    {
+      titulo: "ABM - Trabajo Práctico Facultad",
+      descripcion: "Sistema de gestión (ABM) desarrollado como trabajo práctico universitario.",
+      tecnologias: ["React", "JavaScript", "MySQL", "Axios", "Bootstrap"],
+      imagen: "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?w=500&h=300&fit=crop&crop=center",
+      enlace: "https://proyecto-facultad-abm.netlify.app/inicio"
+    },
 
   ];
 
